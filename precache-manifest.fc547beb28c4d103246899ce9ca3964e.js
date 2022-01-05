51f084ce732b27f45611413a759225f6a522aa7c
vfisher21 <victoria.fisher@yale.edu>
2022-01-05 14:30:08 -0500

    Updates

---
+++ precache-manifest.fc547beb28c4d103246899ce9ca3964e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc81123d6f809f0e8b87",
+    "url": "/VCH_APP_v4/static/css/main.0a15d0b9.chunk.css"
+  },
+  {
+    "revision": "bc81123d6f809f0e8b87",
+    "url": "/VCH_APP_v4/static/js/main.53c6b401.chunk.js"
+  },
+  {
+    "revision": "ce224fad9026b84d2436",
+    "url": "/VCH_APP_v4/static/js/runtime~main.06a63445.js"
+  },
+  {
+    "revision": "dc6bf9b31020aa793219",
+    "url": "/VCH_APP_v4/static/js/2.1f374d9d.chunk.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/VCH_APP_v4/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "b070f1811207348f54b4988fc14bc950",
+    "url": "/VCH_APP_v4/static/media/rating_keydown_1.b070f181.png"
+  },
+  {
+    "revision": "14052abb8ad3bf096d71788ba2e2ace2",
+    "url": "/VCH_APP_v4/static/media/rating_keydown_2.14052abb.png"
+  },
+  {
+    "revision": "bc40f6358057323efebef6ea2db6cb59",
+    "url": "/VCH_APP_v4/static/media/rating_keydown_3.bc40f635.png"
+  },
+  {
+    "revision": "852779dc3fb4af1fd92cb2611fe82806",
+    "url": "/VCH_APP_v4/static/media/rating_keydown_4.852779dc.png"
+  },
+  {
+    "revision": "ae6e6c1fe3fef7ab5aa19a93442d23d3",
+    "url": "/VCH_APP_v4/static/media/rating_keydown_5.ae6e6c1f.png"
+  },
+  {
+    "revision": "8bc03cac713eb041d6a544123de367bf",
+    "url": "/VCH_APP_v4/index.html"
+  }
+];
